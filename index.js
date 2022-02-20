73ee954cbd82ee0e63743f54f1754623fd5fdefa
samax-dx <91656228+samax-dx@users.noreply.github.com>
2022-02-20 20:17:33 +0600

    fixed semantic issues of pipe setup

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.post('/eval', (req, res) => {
     const tappedMethods = req.body.methods.map(method => resTapper(method));
     const topMethodData = req.body.data;
 
-    tappedMethods.unshift([R.identity, R.tap(v => result[v] = v)]);
+    tappedMethods.unshift([R.identity, R.tap(v => result["default"] = v)]);
 
     R.pipe(...tappedMethods.flat())(topMethodData); console.log(result);
 
